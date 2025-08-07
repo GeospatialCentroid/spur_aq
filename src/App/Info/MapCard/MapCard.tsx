de9b89886cbc7329b5768e7efc222ae14ce26f34
kevinworthington <kworthin@gmail.com>
2025-08-07 14:45:25 -0600

    shofted marker iconAnchor

--- src/App/Info/MapCard/MapCard.tsx
+++ src/App/Info/MapCard/MapCard.tsx
@@ -16,7 +16,7 @@ import "./MapCard.css";
 const customIcon = new L.Icon({
   iconUrl: "/Photos/MapCardPhotos/marker-icon-2x.png",
   iconSize: [15, 20],
-  iconAnchor: [16, 32],
+  iconAnchor: [8, 32],
   popupAnchor: [0, -32],
 });
 
