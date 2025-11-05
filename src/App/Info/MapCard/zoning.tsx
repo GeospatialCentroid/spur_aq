c24d13e038d1c31c12d0b5d32f21e4c1cad409e7
Riley Nelson <rileyln1992@gmail.com>
2025-11-05 16:06:06 -0700

    matches the zoning legend on arcgis.com now

--- src/App/Info/MapCard/zoning.tsx
+++ src/App/Info/MapCard/zoning.tsx
@@ -1,32 +1,31 @@
 export type ZoningGroup = { codes: string[]; color: string };
 
 export const zoningGroups: Record<string, ZoningGroup> = {
-  "Airport (DIA)": { codes: ["DIA"], color: "#92C4F0" },
+  "Single Unit (SU)": { codes: ["SU"], color: "#FFFBD5" },
+  "Two Unit (TU)": { codes: ["TU"], color: "#FFF45A" },
+  "Multi Unit (MU, RH, RO)": { codes: ["MU", "RH", "RO"], color: "#FFC94B" },
+  "Residential Mixed Use (RX)": { codes: ["RX"], color: "#E5A64D" },
+  "Commercial Corridor (CC)": { codes: ["CC"], color: "#F4B4A9" },
+  "Mixed Use (MX, M-GMX)": { codes: ["MX", "M-GMX"], color: "#E88A8A" },
+  "Main Street (MS)": { codes: ["MS"], color: "#E55A50" },
+  "Cherry Creek North (C-CCN)": { codes: ["C-CCN"], color: "#FF5C9C" },
+  "Downtown (AS, C, CPV, GT, LD, TD)": { codes: ["AS", "C", "CPV", "GT", "LD", "TD"], color: "#C75A4A" },
+  "Downtown - Civic (D-CV)": { codes: ["D-CV"], color: "#5C8EEB" },
+
+  "Industrial - Light (I-A)": { codes: ["I-A"], color: "#E5D9FA" },
+  "Industrial - General (I-B)": { codes: ["I-B"], color: "#CBB2F5" },
+  "Industrial - Mixed Use (I-MX, M-IMX)": { codes: ["I-MX", "M-IMX"], color: "#C38AF3" },
+
   "Campus (EI, EI2, ENT, H, H2, NWC)": { codes: ["EI", "EI2", "ENT", "H", "H2", "NWC"], color: "#BED2FF" },
-  "Cherry Creek North (C-CCN)": { codes: ["C-CCN"], color: "#FF0066" },
-  "Commercial Corridor (CC)": { codes: ["CC"], color: "#FF9696" },
-  "Downtown (AS, C, CPV, GT, LD, TD)": { codes: ["AS", "C", "CPV", "GT", "LD", "TD"], color: "#C72D26" },
-  "Downtown - Civic (D-CV)": { codes: ["D-CV"], color: "#5685F5" },
-  "Former Chapter 59 Zone": { codes: ["Former Chapter 59 Zone", "CH59"], color: "#E1E1E1" },
+  "Special (O-1)": { codes: ["O-1"], color: "#8DD08A" },
 
-  "Industrial - General (I-B)": { codes: ["I-B"], color: "#D4ABFF" },
-  "Industrial - Light (I-A)": { codes: ["I-A"], color: "#E8C9FF" },
-  "Industrial - Mixed Use (I-MX, M-IMX)": { codes: ["I-MX", "M-IMX"], color: "#FF0000" },
+  "Open Space - Public Parks (OS-A)": { codes: ["OS-A"], color: "#A1EAA0" },
+  "Open Space - Recreation (OS-B)": { codes: ["OS-B"], color: "#B6D592" },
+  "Open Space - Conservation (OS-C)": { codes: ["OS-C"], color: "#88C590" },
 
-  "Main Street (MS)": { codes: ["MS"], color: "#FEFEFA" },
-  "Mixed Use (MX, M-GMX)": { codes: ["MX", "M-GMX"], color: "#E65256" },
-  "Multi Unit (MU, RH, RO)": { codes: ["MU", "RH", "RO"], color: "#FFAA00" },
-  "Manufactured Home Community (MHC)": { codes: ["MHC"], color: "#A87000" },
-
-  "Open Space - Conservation (OS-C)": { codes: ["OS-C"], color: "#73B273" },
-  "Open Space - Public Parks (OS-A)": { codes: ["OS-A"], color: "#96E78A" },
-  "Open Space - Recreation (OS-B)": { codes: ["OS-B"], color: "#9DBA79" },
-
-  "Planned Unit Development (PUD-D, PUD-G)": { codes: ["PUD-D", "PUD-G"], color: "#C8C83C" },
-  "Residential Mixed Use (RX)": { codes: ["RX"], color: "#FF0000" },
-  "Single Unit (SU)": { codes: ["SU"], color: "#FFFFBE" },
-  "Special (O-1)": { codes: ["O-1"], color: "#74C477" },
-  "Two Unit (TU)": { codes: ["TU"], color: "#FFFF00" },
+  "Airport (DIA)": { codes: ["DIA"], color: "#92C4F0" },
+  "Planned Unit Development (PUD-D, PUD-G)": { codes: ["PUD-D", "PUD-G"], color: "#D8D67A" },
+  "Former Chapter 59 Zone": { codes: ["CH59"], color: "#E1E1E1" },
 
   Other: { codes: [], color: "#cccccc" },
 };
